cf21e5e01621295da00136d88431396da064f312
Edison Aviles <eaviles@andrew.cmu.edu>
2023-02-16 17:13:34 -0500

    Added a basic navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react';
 import "../stylesheets/Navbar.css";
 
 function Navbar() {
+    const [showNav, setShowNav] = useState(false);
+    const [navIcon, setNavIcon] = useState(faBars);
+    const updateShowNav = () => {
+        setShowNav(!showNav);
+        if (showNav) {
+            setNavIcon(faBars);
+        } else {
+            setNavIcon(faX);
+        }
+    }
+
+
     return(
         <div className={'navbar-container'} data-testid={'navbar-container'}>
             <h3 className={'title'}>Tactile Chess</h3>
-            <FontAwesomeIcon className={'bars'} icon={faBars} />
+            <FontAwesomeIcon className={`bars`} icon={navIcon} onClick={updateShowNav}/>
+            <div className={`navbar-elements ${showNav && 'active'}`}>
+                <ul>Login</ul>
+                <ul>Share ID</ul>
+                <ul>Create Account</ul>
+                <ul>Home</ul>
+            </div>
         </div>
     )
 }
