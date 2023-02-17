ea65ef0607aefec4a40b9d9e1fd3a1b1919059ad
Edison Aviles <eaviles@andrew.cmu.edu>
2023-02-16 20:14:15 -0500

    installed necessary packages for project, added some chess streams to homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,12 @@
 import Navbar from '../components/Navbar';
+import "../stylesheets/Homepage.css";
 
 function Homepage() {
     return (
         <>
             <Navbar/>
             <div data-testid={"container"} />
+            <iframe src="https://lichess.org/tv/frame?theme=brown&bg=dark" className={"test"} allowtransparency="true" frameborder="0" title={"Test"}></iframe>
         </>
     )
 }
