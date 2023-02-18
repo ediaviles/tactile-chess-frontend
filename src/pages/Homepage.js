5cbb77d466dd983afabc4da36bf522fee004ef7a
Edison Aviles <eaviles4@ford.com>
2023-02-17 19:30:01 -0500

    playing around with api calls

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,6 @@
 import Navbar from '../components/Navbar';
 import "../stylesheets/Homepage.css";
+import axios from "axios";
 
 function Homepage() {
     return (
@@ -7,6 +8,12 @@ function Homepage() {
             <Navbar/>
             <div data-testid={"container"} />
             <iframe src="https://lichess.org/tv/frame?theme=brown&bg=dark" className={"test"} allowtransparency="true" frameborder="0" title={"Test"}></iframe>
+            <button onClick={async () => {
+                await axios.post(
+                    "http://localhost:5000/seekGame"
+                )
+            }}>
+                Create Game</button>
         </>
     )
 }
